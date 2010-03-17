da827ee6953d03dbd1116757305bd81b601d2afb
kcliu <kcliu@kcliu-laptop.(none)>
2010-03-18 02:12:21 +0800

    first version

---
+++ chrome/chromeFiles/content/vocab.js
@@ -0,0 +1,8 @@
+//var Vocab = function() {
+    function onCommand(event) {
+		alert("test");
+		//var selection = document.commandDispatcher.focusedWindow.getSelection().toString();
+		//alert(selection);		
+	}
+//}();
+
