b8e3a05c1563f9e0293956fa2af4d6bd0661bece
kcliu <kcliu@aol.tw>
2010-05-02 16:56:14 +0800

    completed basic DB operations

--- chrome/chromeFiles/content/vocab.js
+++ chrome/chromeFiles/content/vocab.js
@@ -1,31 +1,52 @@
 var vocab = function ( ) {
 	var DBConn;
+	var selection;
 	return {
 		onCommand: function () {
-			var selection = document.commandDispatcher.focusedWindow.getSelection().toString();
-			var stmt;
+			var match;
+			selection = document.commandDispatcher.focusedWindow.getSelection().toString();
 			alert(selection);		
-			alert(this);		
 			this.createDB();
-			//todo "!query"
-	//		if () {
-				this.insertDB(selection);
-	//		} else {
-				//todo "update"
-	//		}
+			match = this.checkExist();
+			alert(match);
+			if ( match === undefined ) {  //new word
+				this.insertDB();
+			} else {
+				this.updateDB(match);     //count++
+			}
 			
 		},
-		insertDB: function (word) {
-			var statement = DBConn.createStatement("INSERT INTO 'vocab' VALUES(?1,1);");  
-			statement.bindStringParameter(0,word);
-			//statement.params.row_id = word;  
-			statement.executeStep();
+		checkExist: function () {
+			var DBstmt;
+			var query;
+			DBstmt = DBConn.createStatement("SELECT counts from vocab where words=:word");
+			DBstmt.params.word = selection;  
+			while ( DBstmt.executeStep() ) {
+				query = DBstmt.row.counts;
+			}
+			return query;
 		},
-		updateDB: function() {
-		
+		insertDB: function () {
+			var DBstmt;
+			//DBstmt = DBConn.createStatement("INSERT INTO 'vocab' VALUES(?1,1);");  
+			//DBstmt.bindStringParameter(0,selection);
+			DBstmt = DBConn.createStatement("INSERT INTO 'vocab' VALUES(:word,1);");  
+			DBstmt.params.word = selection;  
+			DBstmt.executeStep();
+		},
+		updateDB: function(value) {
+			var DBstmt;
+			value++;
+			DBstmt = DBConn.createStatement("UPDATE vocab SET counts =:count WHERE words=:word;");
+			DBstmt.params.word = selection;  
+			DBstmt.params.count = value;  
+			DBstmt.executeStep();
 		},
 		removeDB: function() {
-
+			var DBstmt;
+			DBstmt = DBConn.createStatement("DELETE FROM vocab WHERE words=:word");
+			DBstmt.params.word = selection;  
+			DBstmt.executeStep();
 		},
 		createDB: function() {
 			var file = Components.classes["@mozilla.org/file/directory_service;1"]  
@@ -36,13 +57,11 @@ var vocab = function ( ) {
 								 .getService(Components.interfaces.mozIStorageService);  
 			if ( !file.exists() ) {
 				DBConn = storageService.openDatabase(file); // Will also create the file if it does not exist  
-				DBConn.executeSimpleSQL("CREATE TABLE vocab (words TEXT,count INTEGER);"); 
+				DBConn.createTable("vocab", "words TEXT,counts INTEGER"); 
 			} else {
 				DBConn = storageService.openDatabase(file); // Will also create the file if it does not exist  
 			}
 		},
-		checkDB: function() { 
-		}
 	}
 }();  //kcliu study:: (); means return an object literal
 
